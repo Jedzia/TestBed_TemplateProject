c6e6f6d0f918426a0eb2500cb631a20e116ed1dc
Jedzia <jed69@gmx.de>
2019-10-24 15:23:03 +0200

    src dir

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,8 @@ int main() {
     auto f = 3.14159f;
     std::cout << "Float=" << f << std::endl;
     std::cout << "Float=" << f*6 << std::endl;
+
+    auto lambda = [](auto a) { std::cout << "lambada='" << a << "'." << std::endl; };
+    lambda("I'm da lambda");
     return 0;
 }
