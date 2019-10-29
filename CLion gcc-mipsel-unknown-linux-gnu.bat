7a0bb650fee5dfbbf3c614ab827414ec1692c856
Jedzia <jed69@gmx.de>
2019-10-29 17:00:24 +0100

    refined CLion startup script

--- CLion gcc-mipsel-unknown-linux-gnu.bat
+++ CLion gcc-mipsel-unknown-linux-gnu.bat
@@ -1,4 +1,9 @@
-REM call C:\D\dmd2vars64ldc2.bat
+@title Launching Clion with 'mipsel-unknown-linux-gnu' settings.
+@REM call C:\D\dmd2vars64ldc2.bat
 call setvars-mipsel-unknown-linux-gnu.bat
+@echo starting up Clion ...
 
 start "" "C:\Program Files\JetBrains\CLion 2019.2.3\bin\clion64.exe"
+
+@REM sleep 8 seconds
+@PING localhost -n 9 >NUL
