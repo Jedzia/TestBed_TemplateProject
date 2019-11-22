eacd7920fc4575bb863799b1036a545053c98d9d
Jedzia <jed69@gmx.de>
2019-11-22 01:31:13 +0100

    locales do no work on windoze

--- lib/CBreadBoard/src/display/display.cpp
+++ lib/CBreadBoard/src/display/display.cpp
@@ -7,9 +7,35 @@
 #include <iostream>
 #include <sstream>
 #include <utility>
+#include <mbctype.h>
+
+extern int printLocales();
 
 namespace cbb {
+
+
+    void checkLocales() {
+        return;
+        // does not work on windoze
+        // see https://sourceforge.net/p/mingw/mailman/message/31768770/
+        // The GCC's libstdc++ does not support locales other than the C locale on
+        // MinGW. Somebody has to implement it first.
+        printLocales();
+        std::wcout << "User-preferred locale setting is " << std::locale("").name().c_str() << '\n';
+        // on startup, the global locale is the "C" locale
+        std::wcout << 1000.01 << '\n';
+        // replace the C++ global locale as well as the C locale with the user-preferred locale
+        //std::locale::global(std::locale("en_EN.UTF-8"));
+        std::locale::global(std::locale(u8"en-US.UTF-8"));
+        // use the new global locale for future wide character output
+        std::wcout.imbue(std::locale());
+        // output the same number again
+        std::wcout << 1000.01 << '\n';
+    }
+
     void initDisplay() {
+        checkLocales();
+
         initscr();
         cbreak();
         noecho();
---
+++ lib/CBreadBoard/src/platform/win/printlocales.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Jedzia on 22.11.2019.
+//
+
+
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <ostream>
+#include <iterator>
+
+
+using namespace std;
+
+vector<wstring> locals;
+
+BOOL CALLBACK MyFuncLocaleEx(LPWSTR pStr, DWORD dwFlags, LPARAM lparam) {
+    locals.push_back(pStr);
+    return TRUE;
+}
+
+int printLocales() {
+    EnumSystemLocalesEx(MyFuncLocaleEx, LOCALE_ALL, NULL, NULL);
+
+    for (vector<wstring>::const_iterator str = locals.begin(); str != locals.end(); ++str)
+        wcout << *str << endl;
+
+    wcout << "Total " << locals.size() << " locals found." << endl;
+
+    return 0;
+}
